8b2ffd86948e862c1a120cd36aaab42eea4768da
dgswamy <dgswamy@gmail.com>
2018-07-06 15:32:18 +0530

    string buffer and string decoder is added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http=require('http');
 var url=require('url');
+var stringdecoder=require('string_decoder').stringdecoder;
 var server=http.createServer(function(req,res){
     
     //get url and parse
@@ -8,17 +9,36 @@ var parsedurl=url.parse(req.url,true);
     //get path of the url
 var path=parsedurl.pathname;
 var trimedpath=path.replace(/^\/+|\/+$/g,'');
+//get the query string as an object
+
+var querystringobject=parsedurl.query;
+
 // GET HTTP METHOD
 
 var method=req.method.toLocaleLowerCase();
+//get method as objects
+
+var headers=req.headers;
+
+//get payload if any
+
+var decoder= new stringdecoder('utf-8');
+var buffer='';
+req.on('data',function(data){
+    buffer+=decoder.write(data);
+});
+req.on('end',function(){
+    buffer+= decode.end();
+
+});
 
     //send response
     res.end("hello world");
 
     //log the request path
-console.log("req rcvd on path :"+ trimedpath + 'with method: ' + method);
+//console.log("req rcvd on path :"+ trimedpath + 'with method: ' + method + "with this query string objects : ",querystringobject,'request with these heasders: ',headers);
 
-    
+  console.log("request rcvd with payload : ",buffer);  
     
     
     
